154d3296a9c89db53661f95e8c8f08d5f9234e45
sohan004 <aryansohan02@gmail.com>
2023-05-18 02:34:49 +0600

    home page sec 1 added

--- src/components/HomePage/Sec1/Sec1.jsx
+++ src/components/HomePage/Sec1/Sec1.jsx
@@ -3,7 +3,12 @@ import './Sec1.css'
 
 const Sec1 = () => {
     return (
-        <div></div>
+        <div className='header-bg'>
+            <div className='container py-5 text-start text-md-center head-tx'>
+                <h1 className='display-2 fw-bolder'>Where Imagination Takes Flight - </h1>
+                <h1 className='display-5'>Unleash the Magic of Playtime!</h1>
+            </div>
+        </div>
     );
 };
 
