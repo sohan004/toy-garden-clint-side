7ea18edc36b4506f0c5a4fc9e3e7f9b33cb74ab6
sohan004 <aryansohan02@gmail.com>
2023-05-18 03:39:13 +0600

    home page sec 2 added

--- src/components/HomePage/HomePage.jsx
+++ src/components/HomePage/HomePage.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import Sec1 from './Sec1/Sec1';
+import Sec2 from './Sec2/Sec2';
 
 const HomePage = () => {
     return (
         <div>
             <Sec1></Sec1>
+            <Sec2></Sec2>
         </div>
     );
 };
---
+++ src/components/HomePage/Sec2/Sec2.jsx
@@ -0,0 +1,51 @@
+import React from 'react';
+import img1 from '../../../assets/images (2) (1).png'
+import img2 from '../../../assets/images (2).png'
+import img3 from '../../../assets/images (2) (5).png'
+import img4 from '../../../assets/images (2) (4).png'
+import img5 from '../../../assets/images (2) (3).png'
+import img6 from '../../../assets/images (2) (2).png'
+import './Sec2.css'
+
+const Sec2 = () => {
+    return (
+        <div className='container'>
+            <h1 className="text-center fw-bolder">Toy Guru Gelary</h1>
+            <p className='text-center my-3'>Exploring the Tapestry of Time through Our Gallery</p>
+            <div className="row g-3">
+                <div className="col-6 col-md-7">
+                    <div className='bg p-4 text-center  rounded'>
+                        <img src={img1} alt="" className="rounded-3 img-fluid im " />
+                    </div>
+                </div>
+                <div className="col-6 col-md-5">
+                    <div className='bg p-4 text-center  rounded'>
+                        <img src={img2} alt="" className="rounded-3 img-fluid im" />
+                    </div>
+                </div>
+                <div className="col-6">
+                    <div className='bg p-4 text-center  rounded'>
+                        <img src={img3} alt="" className="rounded-3 img-fluid im" />
+                    </div>
+                </div>
+                <div className="col-6">
+                    <div className='bg p-4 text-center  rounded'>
+                        <img src={img4} alt="" className="rounded-3 img-fluid im " />
+                    </div>
+                </div>
+                <div className="col-6 col-md-5">
+                    <div className='bg p-4 text-center  rounded'>
+                        <img src={img5} alt="" className="rounded-3 img-fluid im" />
+                    </div>
+                </div>
+                <div className="col-6 col-md-7">
+                    <div className='bg p-4 text-center  rounded'>
+                        <img src={img6} alt="" className="rounded-3 img-fluid im" />
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default Sec2;
