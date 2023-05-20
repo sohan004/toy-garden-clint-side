23e0ab0b2a9535ac03cdccad2c2d168e081233eb
sohan004 <aryansohan02@gmail.com>
2023-05-20 21:09:24 +0600

    aos animation adedd

--- src/components/HomePage/Sec2/Sec2.jsx
+++ src/components/HomePage/Sec2/Sec2.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import img1 from '../../../assets/images (2) (1).png'
 import img2 from '../../../assets/images (2).png'
 import img3 from '../../../assets/images (2) (5).png'
@@ -6,41 +6,48 @@ import img4 from '../../../assets/images (2) (4).png'
 import img5 from '../../../assets/images (2) (3).png'
 import img6 from '../../../assets/images (2) (2).png'
 import './Sec2.css'
+import AOS from 'aos';
+import 'aos/dist/aos.css'; // You can also use <link> for styles
+// ..
+
 
 const Sec2 = () => {
+    useEffect(() => {
+        AOS.init({duration: 2000});
+    }, [])
     return (
-        <div className='container'>
+        <div style={{marginBottom: '80px'}} className='container'>
             <h1 className="text-center fw-bolder">Toy Garden Gelary</h1>
             <h5 className='text-center my-3'>Exploring the Tapestry of Time through Our Gallery</h5>
             <div className="row g-3 sc-2">
                 <div className="col-6 col-md-7">
                     <div className='bg p-4 text-center  rounded'>
-                        <img src={img1} alt="" className="rounded-3 img-fluid im " />
+                        <img data-aos="fade-up" src={img1} alt="" className="rounded-3 img-fluid im " />
                     </div>
                 </div>
                 <div className="col-6 col-md-5">
                     <div className='bg p-4 text-center  rounded'>
-                        <img src={img2} alt="" className="rounded-3 img-fluid im" />
+                        <img data-aos="fade-up" src={img2} alt="" className="rounded-3 img-fluid im" />
                     </div>
                 </div>
                 <div className="col-6">
                     <div className='bg p-4 text-center  rounded'>
-                        <img src={img3} alt="" className="rounded-3 img-fluid im" />
+                        <img data-aos="fade-up" src={img3} alt="" className="rounded-3 img-fluid im" />
                     </div>
                 </div>
                 <div className="col-6">
                     <div className='bg p-4 text-center  rounded'>
-                        <img src={img4} alt="" className="rounded-3 img-fluid im " />
+                        <img data-aos="fade-up" src={img4} alt="" className="rounded-3 img-fluid im " />
                     </div>
                 </div>
                 <div className="col-6 col-md-5">
                     <div className='bg p-4 text-center  rounded'>
-                        <img src={img5} alt="" className="rounded-3 img-fluid im" />
+                        <img data-aos="fade-up" src={img5} alt="" className="rounded-3 img-fluid im" />
                     </div>
                 </div>
                 <div className="col-6 col-md-7">
                     <div className='bg p-4 text-center  rounded'>
-                        <img src={img6} alt="" className="rounded-3 img-fluid im" />
+                        <img data-aos="fade-up" src={img6} alt="" className="rounded-3 img-fluid im" />
                     </div>
                 </div>
             </div>
